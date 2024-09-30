a824eb39bb25c8d7f4c3e9159b3aa9304a9e5890
Yoshiaki Hirai <marinos.yoshi@gmail.com>
2024-09-30 16:57:31 +0900

    implemant CORS & lambda access

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,65 @@ type Record = {
   amount: number
 }
 
+type Result = {
+  id: number
+  start: string
+  studio: string
+  instructor: string
+  program: string
+  count: number
+}
+
 function App() {
   const [records, setRecords] = useState<Record[]>([])
   const [title, setTitle] = useState<string>('')
   const [isIncome, setIsIncome] = useState<boolean>(false)
   const [amount, setAmount] = useState<number>(0)
 
+  const [results, setResults] = useState<Result[]>([])
+  const [instructor, setInstructor] = useState<string>('')
+  const [count, setCount] = useState<number>(0)
+
   useEffect(() => {
-      getRecords()
+    getRecords()
 
-      async function getRecords() {
+    async function getRecords() {
 
-        const data_offline = [
-          // 支出と入金のデータを作成
-          {
-            id: 1,
-            title: 'お金を払う',
-            isIncome: false,
-            amount: 1000,
-          }
-        ]
-        const response = await fetch('https://4niabz6kn5itximklhilh5ik340iucxq.lambda-url.ap-northeast-1.on.aws/')
-        const data = await response.json()
-        console.log(data)
-        setRecords(data)
-      }
-  }, [] )
+      const data_offline = [
+        // 支出と入金のデータを作成
+        {
+          id: 1,
+          title: 'お金を払う',
+          isIncome: false,
+          amount: 1000,
+        }
+      ]
+      //      const response = await fetch('https://4niabz6kn5itximklhilh5ik340iucxq.lambda-url.ap-northeast-1.on.aws/',
+      //        {
+      //          mode: "cors",  // クロスオリジンリクエストであることを指定
+      //          credentials: "include",
+      //        }
+      //      )
+      //      const data = await response.json()
+      //      console.log(data)
+      //      setRecords(data)
+
+
+      const response2 = await fetch('https://tl7wjv35kqaysb5z4kf22rnhhe0rexvh.lambda-url.us-east-1.on.aws/',
+        {
+          mode: "cors",  // クロスオリジンリクエストであることを指定
+          credentials: "include",
+        }
+      )
+      const data2 = await response2.json()
+      console.log(data2)
+      setResults(data2)
+    }
+  }, [])
 
   const addRecord = () => {
     const newRecord: Record = {
-      id: records.length+1,
+      id: records.length + 1,
       title: title,
       isIncome: isIncome,
       amount: amount || 0
@@ -54,7 +83,7 @@ function App() {
       <div>
         <Text fontSize='2xl'>家計簿アプリ</Text>
         <Box mb="8px">
-          <Input placeholder='タイトルを入力' mb="4px" onChange={(e) => setTitle(e.target.value)} value={title} /> 
+          <Input placeholder='タイトルを入力' mb="4px" onChange={(e) => setTitle(e.target.value)} value={title} />
           <Input placeholder='支出を入力' mb="4px" onChange={(e) => setAmount(Number(e.target.value))} value={amount} />
           <Flex align="center" justifyContent="space-between">
             <Checkbox w="100px" onChange={() => setIsIncome(!isIncome)} isChecked={isIncome}>入金</Checkbox>
@@ -62,11 +91,11 @@ function App() {
           </Flex>
         </Box>
         <div>
-          {records.map((data) => (
+          {results.map((data) => (
             <div key={data.id}>
               <Flex align="center" justifyContent="space-between">
-                <Text>{data.title}</Text>
-                <Text>{data.isIncome ? "+" : "-"}{data.amount}</Text>
+                <Text>{data.instructor}</Text>
+                <Text>{data.count}</Text>
               </Flex>
             </div>
           ))}
